fce74ceed6c83dd1de8a7adbfa9a4ec92cd43dec
Apolo-punck <71736875+Apolo-punck@users.noreply.github.com>
2020-11-15 17:59:57 -0600

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,12 +1,12 @@
 <?php
- $destino ="apoloholaqase@gmail.com";
+
 $nombre = $_POST["nombre"]
 $correo = $_POST["correo"]
 $mensaje = $_POST["mensaje"]
 $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
 
-mail($destino,"Contacto", $contenido);
-header(Location:index.html)
+mail("apoloholaqase@gmail.com","Contacto", $contenido);
+header("Location:index.html")
 
 
 ?>
